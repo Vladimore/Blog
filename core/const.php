b582fba43671088580ff7015f05502d489a8e9b4
ShowWoodenHorses <vova.vevo666@gmail.com>
2024-06-28 17:15:24 +0300

    upd const.php

--- core/const.php
+++ core/const.php
@@ -2,6 +2,7 @@
 
 define("ROOT", dirname(__DIR__));
 define("PUBLIC", ROOT . "/public");
+define("CONFIG", ROOT . "/config");
 define("CORE", ROOT . "/core");
 define("APP", ROOT . "/app");
 define("CONTROLLERS", APP . "/controllers");
