eb6b8b1eb0cec9c2d7bf9b07612bb02b428669a4
ShowWoodenHorses <vova.vevo666@gmail.com>
2024-07-02 18:35:45 +0300

    upd class Database

--- core/classes/Database.php
+++ core/classes/Database.php
@@ -8,25 +8,60 @@ use PDOException;
 class Database
 {
     private $connect;
+    private $statement;
 
-    public function __construct(array $db_config)
+    private static $instance = null;
+
+    public function __construct()
+    {
+    }
+    public static function getInstance()
+    {
+        if(self::$instance === null){
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(array $db_config)
     {
         $dsn = "mysql:host={$db_config['host']};
         dbname={$db_config['dbname']};
         charset={$db_config['charset']}";
 
         try {
-            $this->connect = new PDO($dsn, $db_config['user'], $db_config['password'], $db_config['option']);
+            $this->connect = new PDO($dsn, $db_config['user'],
+                $db_config['password'],
+                $db_config['option']);
+            return $this;
         }
         catch (PDOException $e) {
             abort(500);
         }
     }
 
-    public function query($query)
+    public function query($query, $params = [])
     {
-        $statement = $this->connect->prepare($query);
-        $statement->execute();
-        return $statement;
+        $this->statement = $this->connect->prepare($query);
+        $this->statement->execute($params);
+        return $this;
+    }
+
+    public function findAll()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+    public function findOrFail()
+    {
+        $res = $this->find();
+        if (!$res){
+            abort();
+        }
+        return $res;
     }
 }
